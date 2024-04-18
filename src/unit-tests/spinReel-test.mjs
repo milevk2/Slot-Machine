578ccb5c1a765542e7b454e87a4671201c47cf92
Mitko Milev <milevk2@gmail.com>
2024-04-18 15:15:27 +0300

    unit tests are now testing the actual Slot class method spinReel

--- src/unit-tests/spinReel-test.mjs
+++ src/unit-tests/spinReel-test.mjs
@@ -1,52 +1,41 @@
 import { expect } from 'chai';
-import spinReel from './functions/spinReel.mjs';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const Slot = require('../../dist/classes/slot.js').default;
 
-describe('spinReel', () => {
-    it('should return correct elements for index 0', () => {
+// initialize object just to have access to the methods. The function parameters will be passed individually with every test:
+const testMachine = new Slot(0,0,[],[],[]);
+
+describe('Tests whether the spinReel function returns the expected visible symbols.', () => {
+    it('should return correct elements for index 0 and 3 rows', () => {
         const reel = [1, 2, 3, 4, 5];
         const index = 0;
         const rowsCount = 3;
-        const result = spinReel(reel, rowsCount, index);
+        const result = testMachine.spinReel(index, rowsCount, reel);
         expect(result).to.deep.equal([1, 2, 3]);
     });
 
-    it('should return correct elements for index 1', () => {
+    it('should return correct elements for last index and 3 rows', () => {
         const reel = [1, 2, 3, 4, 5];
-        const index = 1;
+        const index = reel.length-1;
         const rowsCount = 3;
-        const result = spinReel(reel, rowsCount, index);
-        expect(result).to.deep.equal([2, 3, 4]);
-    });
-
-    it('should return correct elements for index 10', () => {
-        const reel = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-        const index = 10;
-        const rowsCount = 3;
-        const result = spinReel(reel, rowsCount, index);
-        expect(result).to.deep.equal([11, 1, 2]);
-    });
-
-    it('should return correct elements for index reel.length-1', () => {
-        const reel = [1, 2, 3, 4, 5];
-        const index = reel.length - 1;
-        const rowsCount = 3;
-        const result = spinReel(reel, rowsCount, index);
+        const result = testMachine.spinReel(index, rowsCount, reel);
         expect(result).to.deep.equal([5, 1, 2]);
     });
 
-    it('should return correct elements for index reel.length-2', () => {
+    it('should return correct elements for last index and 4 rows', () => {
         const reel = [1, 2, 3, 4, 5];
-        const index = reel.length - 2;
-        const rowsCount = 3;
-        const result = spinReel(reel, rowsCount, index);
-        expect(result).to.deep.equal([4, 5, 1]);
+        const index = reel.length-1;
+        const rowsCount = 4;
+        const result = testMachine.spinReel(index, rowsCount, reel);
+        expect(result).to.deep.equal([5, 1, 2, 3]);
     });
 
-    it('should return correct elements for index reel.length-3', () => {
+    it('should return correct elements for index 0 and 4 rows', () => {
         const reel = [1, 2, 3, 4, 5];
-        const index = reel.length - 3;
-        const rowsCount = 3;
-        const result = spinReel(reel, rowsCount, index);
-        expect(result).to.deep.equal([3, 4, 5]);
+        const index = 0;
+        const rowsCount = 4;
+        const result = testMachine.spinReel(index, rowsCount, reel);
+        expect(result).to.deep.equal([1, 2, 3, 4]);
     });
-});
+})
