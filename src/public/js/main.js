93843661a332b45f84926ac7bad5c53a33d52834
Mitko Milev <milevk2@gmail.com>
2024-04-24 23:57:12 +0300

    clear notifications added. The main.js file is still in progress - most of the functions will be logically separated in another relevant js files

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,5 +1,5 @@
 const symbols = {
-    
+
     1: 'banana',
     2: 'bell',
     3: 'coin',
@@ -12,22 +12,24 @@ const symbols = {
     10: 'watermelon'
 }
 
+document.getElementById('lineBtns').addEventListener('click', paylinesBtnsClick);
 
 async function prepareMachineDom() {
 
     document.querySelector('button.spin').addEventListener('click', spin);
 
-    const {reelsCount, rowsCount, imagesNames} = await getMachineSettings();
+    const { reelsCount, rowsCount, imagesNames, paylines } = await getMachineSettings();
     const slotDiv = document.getElementById('slot');
-    
+
     let currIndex = 0;
 
+    //preparing the rows and columns of the machine:
     for (let row = 1; row <= rowsCount; row++) {
 
         const rowElement = document.createElement('div');
         rowElement.classList.add('row');
 
-        for (let reel = 1;  reel <= reelsCount; reel++) {
+        for (let reel = 1; reel <= reelsCount; reel++) {
 
             const reelElement = document.createElement('div');
             reelElement.classList.add('reel');
@@ -38,13 +40,25 @@ async function prepareMachineDom() {
             if (currIndex > imagesNames.length - 1) currIndex = 0;
 
         }
-
         slotDiv.appendChild(rowElement);
     }
 
+    //preparing the payline buttons:
+
+    const payineBtnsField = document.getElementById('lineBtns');
+
+    for (let i = 0; i < paylines; i++) {
+
+        const button = document.createElement('button');
+        button.className = 'paylineBtn';
+        button.id = `paylineBtn_${i}`;
+        button.textContent = `Payline ${i}`
+        payineBtnsField.appendChild(button);
+
+
+    }
+
     console.log(reelsCount, rowsCount, imagesNames);
-
-
 }
 
 
@@ -56,16 +70,30 @@ async function getMachineSettings() {
     return result;
 }
 
+function clearNotifications() {
+
+    Array.from(document.getElementById('prizeNotification').children).forEach(element => {
+
+        element.remove();
+    })
+}
+
 async function spin() {
 
-    const paylines = [0, 1, 2];
+    clearNotifications();
 
+    const { paylines, id } = JSON.parse(sessionStorage.getItem('machine'))
+
+    console.log(paylines, id);
     const response = await fetch('http://localhost:3000/spin', {
- 
-    method: 'POST',
-    'Content-Type': 'application/json',
-    body: JSON.stringify(paylines)
 
+        method: 'POST',
+
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({ paylines, id })
     })
 
     if (!response.ok) {
@@ -73,16 +101,30 @@ async function spin() {
         throw new Error('Something went wrong with the request!')
     }
 
-    const visibleReels = await response.json();
+    const spinResult = await response.json();
 
-    updateVisibleReels(visibleReels);
+    getScore();
+    updateVisibleReels(spinResult.visibleReels);
+    displayPrizeNotifications(spinResult.prizeNotifications);
+}
+
+function displayPrizeNotifications(notifications) {
+
+    const messageField = document.getElementById('prizeNotification');
+
+    for (let message of notifications) {
+
+        const paragraph = document.createElement('p');
+        paragraph.textContent = message;
+        messageField.appendChild(paragraph);
+    }
 }
 
 function updateVisibleReels(result) {
 
     const visibleReels = Array.from(document.querySelectorAll('div.row')).map(el => {
 
-       return Array.from( el.children);
+        return Array.from(el.children);
 
     })
 
@@ -96,6 +138,95 @@ function updateVisibleReels(result) {
 }
 
 
+async function prepareMachineCache() {
 
-prepareMachineDom()
+    const response = await fetch('http://localhost:3000/createMachine');
+    const id = await response.json();
+
+    sessionStorage.setItem('machine', JSON.stringify({
+
+        id,
+        totalPrize: 0,
+        totalWins: 0,
+        paylines: [],
+        currentPrize: 0,
+
+    }))
+}
+
+async function getScore() {
+
+    const response = await fetch('http://localhost:3000/score');
+    const { totalWins, totalPrize, scoreString } = await response.json();
+
+    updateScore(totalWins, totalPrize)
+    displayScore(scoreString);
+}
+
+function updateScore(totalWins, totalPrize) {
+
+    const cachedScore = JSON.parse(sessionStorage.getItem('machine'));
+
+    cachedScore['totalWins'] = totalWins;
+    cachedScore['totalPrize'] = totalPrize;
+
+    sessionStorage.setItem('machine', JSON.stringify(cachedScore));
+
+}
+
+async function displayScore(score) {
+
+    document.getElementById('score').textContent = score;
+
+}
+
+function paylinesBtnsClick(e) {
+
+    const id = e.target.id;
+    if (id == 'lineBtns') return;
+ 
+    const paylines = Array.from(JSON.parse(sessionStorage.getItem('machine')).paylines);
+
+    if (!paylines.includes(id.slice(-1))){
+
+        subscribeToPayline(id.slice(-1));     
+    }
+  
+   
+
+    if (id == 'paylineBtn_0') {
+
+        document.querySelector('.row').classList.toggle('yellowBorder');
+
+
+    }
+}
+
+async function subscribeToPayline(index) {
+
+    const machineId = JSON.parse(sessionStorage.getItem('machine')).id;
+
+    const response = await fetch('http://localhost:3000/subscribe', {
+
+        method: 'POST',
+
+        headers: {
+
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({ index, machineId })
+
+    })
+
+    if (!response.ok) throw new Error('Something went wrong with the request!')
+
+    const result = await response.json();
+
+    console.log(result);
+
+}
+
+prepareMachineCache();
+prepareMachineDom();
 
