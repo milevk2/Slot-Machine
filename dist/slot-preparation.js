d2d41fa87c6f253d0c3412897355f86b4e4cf7f1
Mitko Milev <milevk2@gmail.com>
2024-04-24 23:38:41 +0300

    slot object initialization, patternt initialization

--- dist/slot-preparation.js
+++ dist/slot-preparation.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.subscriptions = exports.slotMachine = void 0;
 const slot_1 = __importDefault(require("./classes/slot"));
 const ZigZag_1 = __importDefault(require("./classes/patterns/ZigZag"));
 const Line_1 = __importDefault(require("./classes/patterns/Line"));
@@ -14,7 +15,6 @@ const lineTwoPattern = new Line_1.default(2, configuration_1.default.lines[2]);
 const zeroOnePattern = new ZigZag_1.default(3, configuration_1.default.lines[3]);
 const oneTwoPattern = new ZigZag_1.default(4, configuration_1.default.lines[4]);
 const subscriptions = [lineZeroPattern, lineOnePattern, lineTwoPattern, zeroOnePattern, oneTwoPattern];
+exports.subscriptions = subscriptions;
 const slotMachine = new slot_1.default(configuration_1.default.reelsCount, configuration_1.default.rowsCount, configuration_1.default.symbols, configuration_1.default.lines, configuration_1.default.reels, subscriptions);
-//displaying the total results - total wins and total accumulated prize:
-//slotMachine.displayScore();
-exports.default = slotMachine;
+exports.slotMachine = slotMachine;
