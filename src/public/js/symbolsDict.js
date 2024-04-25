e5e827702702030b4d39eb7cd3570132962c4fe0
Mitko Milev <milevk2@gmail.com>
2024-04-26 01:28:15 +0300

    symbols dictionary is now in a separate file

---
+++ src/public/js/symbolsDict.js
@@ -0,0 +1,15 @@
+const symbols = {
+
+    1: 'banana',
+    2: 'bell',
+    3: 'coin',
+    4: 'diamond',
+    5: 'grape',
+    6: 'heart',
+    7: 'leef',
+    8: 'orange',
+    9: 'seven',
+    10: 'watermelon'
+}
+
+export default symbols;
